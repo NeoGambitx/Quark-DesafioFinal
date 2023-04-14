a8ae394ccb5edbe08482fa6e315a0023d1990f10
NeoGambitx <federicobertachi@hotmail.com>
2023-04-14 08:38:33 -0300

    Terminado

--- modelo/headers/prenda.h
+++ modelo/headers/prenda.h
@@ -11,10 +11,12 @@ class Prenda{
         Prenda(std::string calidad, int stock, float precio);
         int getStock();
         virtual float getPrecioUnitario() = 0; //Cada subClase tiene su propia implementación (Camisa, Pantalon)
+        virtual std::string getCaracteristicas() = 0;
         void setStock(int nuevoStock);
         void setPrecioUnitario(float precioBase);
         void setCalidad(std::string c);
         std::string getCalidad();
+
 };
 
 //Fin Header Prenda.h
